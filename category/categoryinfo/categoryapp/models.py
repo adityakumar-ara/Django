47445eb1fa9e36a042aeca5af50562a5276242c9
Aditya Kumar <adityaara7667@gmail.com>
2026-04-11 18:37:25 +0530

    add more

---
+++ category/categoryinfo/categoryapp/models.py
@@ -0,0 +1,11 @@
+
+# Create your models here.
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
