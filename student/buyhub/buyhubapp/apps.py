6049bf96ffb26436df631aa954cadd87e8869fd0
Aditya Kumar <adityaara7667@gmail.com>
2026-04-13 22:01:06 +0530

    update

---
+++ student/buyhub/buyhubapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BuyhubappConfig(AppConfig):
+    name = 'buyhubapp'
--- student/studentinfo/studentinfoapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StudentinfoappConfig(AppConfig):
-    name = 'studentinfoapp'
